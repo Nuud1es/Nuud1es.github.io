6236a582104629ac8c50d095924b0ad87c65551b
Nuud1es <42973862+Nuud1es@users.noreply.github.com>
2024-12-05 19:25:02 +0000

    change to portfolio website

--- script.js
+++ script.js
@@ -1,56 +1,132 @@
-const thumbs = document.querySelectorAll('.thumb li');
-const infoSlider = document.querySelectorAll('.info-slider');
-const items = document.querySelectorAll('.item');
+'use strict';
 
-let index = 0;
+//Opening or closing side bar
 
-thumbs.forEach((thumb, ind) => {
-    thumb.addEventListener('click', (event) => {
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-        setTimeout(() => {
-            document.querySelector('.thumb .selected').classList.remove('selected');
-            thumb.classList.add('selected');
-        }, 100);
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-        thumbs.forEach(thum => {
-            thum.classList.add('disabled');
-            setTimeout(() => {
-                thum.classList.remove('disabled');
-            }, 100);
-        });
+sidebarBtn.addEventListener("click", function() {elementToggleFunc(sidebar); })
 
-        let anySelected = false;
-        let current = event.target.parentElement.nextElementSibling;
+//Activating Modal-testimonial
 
-        while(current !== null && !anySelected){
-            anySelected = anySelected || current.matches('.selected');
-            current = current.nextElementSibling
+const testimonialsItem = document.querySelectorAll('[data-testimonials-item]');
+const modalContainer = document.querySelector('[data-modal-container]');
+const modalCloseBtn = document.querySelector('[data-modal-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
 
-            if(anySelected){
-                items[index].classList.add('previously-active');
+const modalImg = document.querySelector('[data-modal-img]');
+const modalTitle = document.querySelector('[data-modal-title]');
+const modalText = document.querySelector('[data-modal-text]');
 
-                setTimeout(() => {
-                    document.querySelector('.item.previously-active').classList.remove('previously-active');
-                }, 100);
+const testimonialsModalFunc = function () {
+    modalContainer.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
 
-                index = ind;
-                items[index].classList.add('back-active');
+for (let i = 0; i < testimonialsItem.length; i++) {
+    testimonialsItem[i].addEventListener('click', function () {
+        modalImg.src = this.querySelector('[data-testimonials-avatar]').src;
+        modalImg.alt = this.querySelector('[data-testimonials-avatar]').alt;
+        modalTitle.innerHTML = this.querySelector('[data-testimonials-title]').innerHTML;
+        modalText.innerHTML = this.querySelector('[data-testimonials-text]').innerHTML;
 
-                setTimeout(() => {
-                    document.querySelector('.item.back-active').classList.remove('back-active');
-                }, 100);
-            }
-        }
+        testimonialsModalFunc();
+    })
+}
 
-        index = ind;
+//Activating close button in modal-testimonial
 
-        infoSlider.forEach(slide => {
-            slide.style.transform = `translateY(${index * -100}%)`;
-        });
+modalCloseBtn.addEventListener('click', testimonialsModalFunc);
+overlay.addEventListener('click', testimonialsModalFunc);
 
-        document.querySelector('.item.active').classList.remove('active');
-        items[index].classList.add('active');
+//Activating Filter Select and filtering options
+
+const select = document.querySelector('[data-select]');
+const selectItems = document.querySelectorAll('[data-select-item]');
+const selectValue = document.querySelector('[data-select-value]');
+const filterBtn = document.querySelectorAll('[data-filter-btn]');
+
+select.addEventListener('click', function () {elementToggleFunc(this); });
+
+for(let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener('click', function() {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        elementToggleFunc(select);
+        filterFunc(selectedValue);
 
     });
-});
+}
 
+const filterItems = document.querySelectorAll('[data-filter-item]');
+
+const filterFunc = function (selectedValue) {
+    for(let i = 0; i < filterItems.length; i++) {
+        if(selectedValue == "all") {
+            filterItems[i].classList.add('active');
+        } else if (selectedValue == filterItems[i].dataset.category) {
+            filterItems[i].classList.add('active');
+        } else {
+            filterItems[i].classList.remove('active');
+        }
+    }
+}
+
+//Enabling filter button for larger screens 
+
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+    
+    filterBtn[i].addEventListener('click', function() {
+
+        let selectedValue = this.innerText.toLowerCase();
+        selectValue.innerText = this.innerText;
+        filterFunc(selectedValue);
+
+        lastClickedBtn.classList.remove('active');
+        this.classList.add('active');
+        lastClickedBtn = this;
+
+    })
+}
+
+// Enabling Contact Form
+
+const form = document.querySelector('[data-form]');
+const formInputs = document.querySelectorAll('[data-form-input]');
+const formBtn = document.querySelector('[data-form-btn]');
+
+for(let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener('input', function () {
+        if(form.checkValidity()) {
+            formBtn.removeAttribute('disabled');
+        } else { 
+            formBtn.setAttribute('disabled', '');
+        }
+    })
+}
+
+// Enabling Page Navigation 
+
+const navigationLinks = document.querySelectorAll('[data-nav-link]');
+const pages = document.querySelectorAll('[data-page]');
+
+for(let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function() {
+        
+        for(let i = 0; i < pages.length; i++) {
+            if(this.innerHTML.toLowerCase() == pages[i].dataset.page) {
+                pages[i].classList.add('active');
+                navigationLinks[i].classList.add('active');
+                window.scrollTo(0, 0);
+            } else {
+                pages[i].classList.remove('active');
+                navigationLinks[i]. classList.remove('active');
+            }
+        }
+    });
+}
